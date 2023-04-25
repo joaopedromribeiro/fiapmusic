174d54c534bd81da416ed4727890e53ad7aa6bb5
João Pedro Ribeiro <98784678+joaopedromribeiro@users.noreply.github.com>
2023-04-25 16:32:07 -0300

    Update planos.js

--- JS/planos.js
+++ JS/planos.js
@@ -1,16 +1,52 @@
-document.querySelector("#qtde").addEventListener("change", calcularOrcamento)
-document.querySelector("#js").addEventListener("change", calcularOrcamento)
-document.querySelector("#maior_sim").addEventListener("change", calcularOrcamento)
-document.querySelector("#maior_nao").addEventListener("change", calcularOrcamento)
-document.querySelector("#Meses").addEventListener("change", calcularOrcamento)
+// Captura os elementos por seus ids
+const selectPlanos = document.getElementsByName("Planos")[0];
+const rangeMeses = document.getElementById("Meses");
+const output = document.getElementById("output");
 
-function calcularOrcamento() {
-    
-    if (js.option) "Individual"
-    let valor_plano = preco * (1.1 - Meses.value * 0.1)
-    preco += taxaDeUrgencia
 
-    label_prazo.innerHTML = `Prazo (${Meses.value} semanas)`
+// Adiciona um evento de inicialização ao range
+rangeMeses.addEventListener("load", () => {
+  // Define o valor do range com o seu valor mínimo
+  rangeMeses.value = rangeMeses.min;
 
-    output.innerHTML = "R$ " + preco.toFixed(2)
+  // Seleciona o valor "Individual" no select
+  selectPlanos.value = "Individual";
+
+  // Calcula o valor total com base no plano inicial
+  calcularValorTotal();
+});
+
+// Adiciona um evento de mudança ao range
+rangeMeses.addEventListener("change", calcularValorTotal);
+
+// Adiciona um evento de mudança ao select
+selectPlanos.addEventListener("change", calcularValorTotal);
+
+// Função de cálculo de valor total
+function calcularValorTotal() {
+  // Obtém o valor selecionado no range
+  const meses = rangeMeses.value;
+
+  // Obtém o preço atual do plano selecionado no select
+  let preco = 0;
+
+  switch (selectPlanos.value) {
+    case "Individual":
+      preco = 15.9;
+      break;
+    case "Familiar":
+      preco = 29.9;
+      break;
+    case "Universitário":
+      preco = 9.9;
+      break;
+    default:
+      preco = 0;
+  }
+
+  // Calcula o valor total baseado no preço do plano e no número de meses
+  const valorTotal = preco * meses;
+
+  // Atualiza o valor do output com o valor total formatado
+  output.value = `R$ ${valorTotal.toFixed(2)}`;
 }
